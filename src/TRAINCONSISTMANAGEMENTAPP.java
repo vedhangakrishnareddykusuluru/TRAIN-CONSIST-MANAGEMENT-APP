d0a7fdb1115a0675a53a0baae5081db484496dea
vedhangakrishnareddykusuluru <vk4872@srmist.edu.in>
2026-03-27 15:34:59 +0530

    UC3: Track Unique Bogie IDs (Set – HashSet)

--- src/TRAINCONSISTMANAGEMENTAPP.java
+++ src/TRAINCONSISTMANAGEMENTAPP.java
@@ -1,19 +1,22 @@
+import java.util.*;
 public class TRAINCONSISTMANAGEMENTAPP {
     public static void main (String args[]){
         System.out.println("==========================================");
-        System.out.println("   UC2 - Add Passenger Bogie to Train   ");
+        System.out.println("   UC3 - Add Passenger Bogie IDs  ");
         System.out.println("==========================================");
+        Set <String> bogies = new HashSet<>();
+        bogies.add("BG101");
+        bogies.add("BG102");
+        bogies.add("BG103");
+        bogies.add("BG104");
+        bogies.add("BG101");
+        bogies.add("BG102");
+        System.out.println("Bogie IDs After Insertion:"+bogies);
         System.out.println();
-        System.out.println("After Adding Bogies:");
-        System.out.println("Passenger Bogie : [Sleeper, AC Chair, First Class");
+        System.out.println("Note:");
+        System.out.println("Duplicate are automatically ignored by HashSet.");
         System.out.println();
-        System.out.println("Checking if 'Sleeper' exists:");
-        System.out.println("Contains Sleeper? : true");
-        System.out.println();
-        System.out.println("Final Train Passenger Consist:");
-        System.out.println("[Sleeper, First Class");
-        System.out.println();
-        System.out.println("UC2 operations completed successfully...");
+        System.out.println("UC3 uniqueness validation completed...");
 
 
     }
