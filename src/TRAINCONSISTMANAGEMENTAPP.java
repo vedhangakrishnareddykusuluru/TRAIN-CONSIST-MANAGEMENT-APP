d13ee5eca638f98a19ff8e9787aa722c7d588984
vedhangakrishnareddykusuluru <vk4872@srmist.edu.in>
2026-03-27 14:50:30 +0530

    Base application setup

---
+++ src/TRAINCONSISTMANAGEMENTAPP.java
@@ -0,0 +1,15 @@
+public class TRAINCONSISTMANAGEMENTAPP {
+    public static void main (String args[]){
+        System.out.println("==========================================");
+        System.out.println("   === Train Consist Management App ===   ");
+        System.out.println("==========================================");
+        System.out.println();
+        System.out.println("Train initialized successfully...");
+        System.out.println("Initial Bogie Count : 0");
+        System.out.println("Current Train Consist :[]");
+        System.out.println();
+        System.out.println("System ready for operations...");
+
+    }
+
+}
